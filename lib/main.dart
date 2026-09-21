81be7f5fd65a137f6d1fd18e7a5ec335e43d4da6
Malaviya Parth Mahendrabhai <parthmalaviya567@gmail.com>
2026-09-21 17:09:49 +0530

    Database configuration added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+import 'firebase_options.dart';
 import 'services/car_service.dart';
 import 'screens/home_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const CarRentalApp());
 }
 
